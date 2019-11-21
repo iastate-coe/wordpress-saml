a752b076c7371f2500b63e34a0ecc8b0fa77ea19
Kevin Wickham <kwickham@iastate.edu>
2019-11-21 13:42:04 -0600

    fix alt acs path

--- acs.php
+++ acs.php
@@ -1,6 +1,5 @@
 <?php
-#require_once dirname(dirname(__FILE__)) . '/wp-load.php';
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/public/wp-load.php';
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php';
 require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/functions.php";
 require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/configuration.php";
 
