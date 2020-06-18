6fca35805b31c218187a78f224126269b3c01968
Kevin Wickham <kwickham@iastate.edu>
2020-06-18 16:41:12 -0500

    load plugin if loaded on sub-site

--- alternative_acs.php
+++ alternative_acs.php
@@ -4,16 +4,13 @@ require_once dirname(__FILE__, 4) . '/wp-load.php';
 require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/functions.php";
 require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/configuration.php";
 
-/*
-function acs_shutdown_handle(){
-    wp_die('','SAML Error', array(
-        //'link_url' => '#',
-        //'link_text' => 'link',
-        //'back_link' => true,
-        'exit' => false,
-    ));
+$blog = get_site_by_path('www.scotchbox.local',$_REQUEST['RelayState']);
+if($blog instanceof WP_Site){
+    switch_to_blog($blog->blog_id);
 }
-register_shutdown_function('acs_shutdown_handle');
-*/
+$is_active = is_plugin_active( 'isu-okta-privacy/isu-okta-privacy.php');
+if ($is_active) {
+    include_once plugin_dir_path(dirname(__FILE__))."isu-okta-privacy/isu-okta-privacy.php";
+}
 
 saml_acs();
