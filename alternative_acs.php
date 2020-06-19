6ea84dd40a1d6f44f9e6d1887f5ae9576b643235
Kevin Wickham <kwickham@iastate.edu>
2020-06-19 13:09:22 -0500

    update plugin name

--- alternative_acs.php
+++ alternative_acs.php
@@ -8,7 +8,7 @@ $blog = get_site_by_path('www.scotchbox.local',$_REQUEST['RelayState']);
 if($blog instanceof WP_Site){
     switch_to_blog($blog->blog_id);
 }
-$is_active = is_plugin_active( 'isu-okta-privacy/isu-okta-privacy.php');
+$is_active = is_plugin_active( 'okta-privacy/okta-privacy.php');
 if ($is_active) {
     include_once plugin_dir_path(dirname(__FILE__))."isu-okta-privacy/isu-okta-privacy.php";
 }
