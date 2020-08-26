d1a16b6f4e607d7a9b77479a2acd02c75a09097f
Kevin Wickham <kwickham@iastate.edu>
2020-08-26 12:58:09 -0500

    fix plugin tie

--- alternative_acs.php
+++ alternative_acs.php
@@ -1,16 +1,19 @@
 <?php
 require_once dirname(__FILE__, 4) . '/wp-load.php';
-#require_once dirname(__FILE__,4) . '/public/wp-load.php';
-require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/functions.php";
-require_once plugin_dir_path(__FILE__)."onelogin-saml-sso/php/configuration.php";
+#require_once dirname(__FILE__, 4) . '/public/wp-load.php';
+require_once plugin_dir_path(__FILE__) . "onelogin-saml-sso/php/functions.php";
+require_once plugin_dir_path(__FILE__) . "onelogin-saml-sso/php/configuration.php";
 
-$blog = get_site_by_path('www.scotchbox.local',$_REQUEST['RelayState']);
-if($blog instanceof WP_Site){
+$serverHost = filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING);
+$blog = get_site_by_path($serverHost, $_REQUEST['RelayState']);
+if ($blog instanceof WP_Site) {
     switch_to_blog($blog->blog_id);
 }
-$is_active = is_plugin_active( 'okta-privacy/okta-privacy.php');
-if ($is_active) {
-    include_once plugin_dir_path(dirname(__FILE__))."isu-okta-privacy/isu-okta-privacy.php";
+if (function_exists('is_plugin_active')) {
+    $is_active = is_plugin_active('okta-privacy/okta-privacy.php');
+    if ($is_active) {
+        include_once plugin_dir_path(dirname(__FILE__)) . "okta-privacy/okta-privacy.php";
+    }
 }
 
 saml_acs();
