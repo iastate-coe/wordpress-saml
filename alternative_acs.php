586232ec8f548fcb94f497eb9d89d3464b29be08
Kevin Wickham <kwickham@iastate.edu>
2021-02-11 14:00:48 -0600

    back compat

--- alternative_acs.php
+++ alternative_acs.php
@@ -39,6 +39,13 @@ if ( $is_active ) {
 	run_wordpress_saml_private_blog();
 }
 
+$okta_privacy_plugin = 'okta-privacy/okta-privacy.php';
+$is_active           = in_array( $okta_privacy_plugin, (array) get_option( 'active_plugins', array() ), true );
+
+if ( $is_active ) {
+	include_once plugin_dir_path( dirname( __FILE__ ) ) . $okta_privacy_plugin;
+	new OktaPrivateISU();
+}
 /*
  * ------------------------------------------------------------------------------------------------------
  * Run WordPress SAML acs endpoint.
